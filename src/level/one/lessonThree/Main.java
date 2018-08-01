3ecd469b16ed051759a06348c2ee27b55c088301
elendi007 <elendi@inbox.ru>
2018-08-01 20:14:08 +0300

    выполнил третье задание.

---
+++ src/level/one/lessonThree/Main.java
@@ -0,0 +1,9 @@
+package level.one.lessonThree;
+
+public class Main {
+    public static void main(String[] args){
+        for (int i = 0; i < 16; i++) {
+            System.out.println("Хочешь решай задачи, не хочешь - решай нехотя");
+        }
+    }
+}
