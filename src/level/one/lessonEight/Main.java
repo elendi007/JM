045b7348f5088668965208f66644d146a147fc9c
elendi007 <elendi@inbox.ru>
2018-08-01 22:24:11 +0300

    выполнил восьмое задание.

---
+++ src/level/one/lessonEight/Main.java
@@ -0,0 +1,16 @@
+package level.one.lessonEight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.print("Введите целое число: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        System.out.println("Квадрат считанного числа равен: " + (a * a));
+    }
+}
