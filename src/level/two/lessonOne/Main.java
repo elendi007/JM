c895be3cf640e28901dc2fd95b386cc82c9d6f8a
elendi007 <elendi@inbox.ru>
2018-08-02 18:06:36 +0300

    выполнил первое задание второго уровня.

---
+++ src/level/two/lessonOne/Main.java
@@ -0,0 +1,16 @@
+package level.two;
+
+import java.util.Scanner;
+
+public class lessonOne {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+
+        System.out.println("количество символов в строке: " + a.length());
+
+        
+    }
+}
