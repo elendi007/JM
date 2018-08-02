27d0303390d4ffd59a26ed2bde1cadd2e9a3f448
elendi007 <elendi@inbox.ru>
2018-08-02 18:08:07 +0300

    выполнил первое задание второго уровня. (повторный комит, ошибка с гитом)

--- src/level/two/lessonOne/Main.java
+++ src/level/two/lessonOne/Main.java
@@ -1,16 +1,18 @@
-package level.two;
+package level.two.lessonOne;
 
 import java.util.Scanner;
 
-public class lessonOne {
+public class Main {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите строку: ");
 
         String a = scanner.nextLine();
 
         System.out.println("количество символов в строке: " + a.length());
 
-        
+        System.out.println("введёная строка: " + a + " java");
     }
 }
