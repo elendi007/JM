bffbd180a6ed28193f0f0e9d052deb64424b5e03
elendi2003 <elendi2003@hotmail.com>
2018-08-04 23:50:55 +0300

    выполнил пятое задание из третьего уровня.

---
+++ src/level/three/lessonFive/Main.java
@@ -0,0 +1,39 @@
+package level.three.lessonFive;
+
+public class Main {
+    public static void main(String[] args){
+        String[] roles = new String[4];
+        roles[0] = "Городничий";
+        roles[1] = "Аммос Федорович";
+        roles[2] = "Артемий Филиппович";
+        roles[3] = "Лука Лукич";
+
+        String[] textLines = new String[7];
+        textLines[0] = "Городничий: Я пригласил вас, господа, с тем," +
+                "чтобы сообщить вам пренеприятное известие: к нам едет ревизор.";
+        textLines[1] = "Аммос Федорович: Как ревизор?";
+        textLines[2] = "Артемий Филиппович: Как ревизор?";
+        textLines[3] = "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.";
+        textLines[4] = "Аммос Федорович: Вот те на!";
+        textLines[5] = "Артемий Филиппович: Вот не было заботы, так подай!";
+        textLines[6] = "Лука Лукич: Господи боже! еще и с секретным предписаньем!";
+
+        System.out.println(printTextPerRole(roles, textLines));
+    }
+
+    private static String printTextPerRole(String[] roles, String[] textLines) {
+        StringBuilder textPerRole = new StringBuilder();
+
+        for (int i = 0; i < roles.length; i++) {
+            textPerRole.append(roles[i] + ":\n");
+            for (int j = 0; j < textLines.length; j++) {
+                if(textLines[j].startsWith(roles[i] + ":")){
+                    textPerRole.append(j + ") " + textLines[j].substring(1 + textLines[j].indexOf(':')) + "\n");
+                }
+            }
+            textPerRole.append("\n");
+        }
+
+        return textPerRole.toString();
+    }
+}
