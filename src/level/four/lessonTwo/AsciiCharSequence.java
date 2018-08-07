5eb9ececd0f5ecded51fb51d0a08e06fb34aa4e7
elendi2003 <elendi2003@hotmail.com>
2018-08-08 00:10:28 +0300

    перешёл ко второй задаче четвёртого уровня.

---
+++ src/level/four/lessonTwo/AsciiCharSequence.java
@@ -0,0 +1,28 @@
+package level.four.lessonTwo;
+
+public class AsciiCharSequence implements CharSequence {
+
+    AsciiCharSequence(byte[] bytes_array){
+
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/level/four/lessonTwo/Main.java
@@ -0,0 +1,7 @@
+package level.four.lessonTwo;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
