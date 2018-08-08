2730bc7e1eeae59af87b39682026c8d9ed5dc08d
elendi007 <elendi@inbox.ru>
2018-08-08 21:52:53 +0300

    выполнил вторую задачу четвёртого уровня.

--- src/level/four/lessonTwo/AsciiCharSequence.java
+++ src/level/four/lessonTwo/AsciiCharSequence.java
@@ -2,27 +2,46 @@ package level.four.lessonTwo;
 
 public class AsciiCharSequence implements CharSequence {
 
-    AsciiCharSequence(byte[] bytes_array){
+    private byte [] byte_array;
 
+    AsciiCharSequence(byte[] bytes_array){
+        this.byte_array = bytes_array;
     }
 
     @Override
     public int length() {
-        return 0;
+        return this.byte_array.length;
     }
 
+    /**
+     * @param index
+     * @return символ, расположенный по указанному индексу строки. Индексы строк в Java начинаются с нуля.
+     */
     @Override
     public char charAt(int index) {
-        return 0;
+        return (char) byte_array[index];
     }
 
+    /**
+     * @param start
+     * @param end
+     * @return новую последовательность символов,
+     * которая является подпоследовательностью этой последовательности,
+     * другими словами — возвращает последовательность символов из строки, начиная и заканчивая указанными индексами.
+     */
     @Override
     public CharSequence subSequence(int start, int end) {
-        return null;
+        byte[] assignedSequence = new byte[end - start];
+        System.arraycopy(byte_array, start, assignedSequence, 0, end - start);
+        return new AsciiCharSequence(assignedSequence);
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < byte_array.length; i++) {
+            stringBuilder.append((char) byte_array[i]);
+        }
+        return stringBuilder.toString();
     }
 }
--- src/level/four/lessonTwo/Main.java
+++ src/level/four/lessonTwo/Main.java
@@ -2,6 +2,9 @@ package level.four.lessonTwo;
 
 public class Main {
     public static void main(String[] args){
+        byte[] sequence = {48,49,50,51,52,53,54,55};
+        AsciiCharSequence asciiCharSequence = new AsciiCharSequence(sequence);
 
+        System.out.println(asciiCharSequence.subSequence(0,3));
     }
 }
